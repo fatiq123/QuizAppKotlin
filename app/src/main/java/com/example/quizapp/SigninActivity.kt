798a95a458967ee0c3dbc9331b78dd1d14d40c4f
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-15 09:15:51 -0700

    designing the layout for signin page and signup page

--- app/src/main/java/com/example/quizapp/SigninActivity.kt
+++ app/src/main/java/com/example/quizapp/SigninActivity.kt
@@ -1,11 +1,24 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.quizapp.databinding.ActivitySigninBinding
+import com.example.quizapp.databinding.ActivitySignupBinding
 
 class SigninActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signin)
+
+
+
+        binding.buttonNext.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/quizapp/SignupActivity.kt
+++ app/src/main/java/com/example/quizapp/SignupActivity.kt
@@ -1,11 +1,23 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.quizapp.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_signup)
+
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SigninActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
