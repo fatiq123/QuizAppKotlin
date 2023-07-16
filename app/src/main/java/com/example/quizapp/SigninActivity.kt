58f29aae06dc597783f438173cde428f5970ffb7
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-15 21:36:25 -0700

    firebase authentication dependency added for project

--- app/src/main/java/com/example/quizapp/SigninActivity.kt
+++ app/src/main/java/com/example/quizapp/SigninActivity.kt
@@ -6,13 +6,35 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.quizapp.databinding.ActivitySigninBinding
 import com.example.quizapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SigninActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySigninBinding
+
+    // Authentication
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_signin)
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -20,6 +42,20 @@ class SigninActivity : AppCompatActivity() {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+    }
+
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    private fun reload() {
 
     }
 }
