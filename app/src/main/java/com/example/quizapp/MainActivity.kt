645c24d30b9feaa7c21de0b4cc2e718aecd9429c
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-19 01:00:45 -0700

    errors for actionBar

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,32 +1,82 @@
 package com.example.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.quizapp.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
+    private lateinit var customToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
-        navController = findNavController(R.id.fragmentContainerView)
+//        // Set the toolbar as the ActionBar
+//        val toolbar: Toolbar = findViewById(R.id.toolbar)
+//        setSupportActionBar(toolbar)
+
+        // Find the custom Toolbar by ID
+        customToolbar = findViewById(R.id.customToolbar)
+        setSupportActionBar(customToolbar)
+
+
+//        navController = findNavController(R.id.fragmentContainerView)
+//        setupActionBarWithNavController(navController)
+
+
+        // Find the NavHostFragment using its ID
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        // Get the NavController from the NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the ActionBar with the NavController
         setupActionBarWithNavController(navController)
 
 
+
+//        // Set up the Up button (Back button)
+//        val actionBar = supportActionBar
+//        if (actionBar != null) {
+//            actionBar.setDisplayHomeAsUpEnabled(true)
+//        }
     }
 
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
+
+        // Get the ID of the current destination
+        val currentDestinationId = navController.currentDestination?.id
+
+        // Hide the Toolbar (ActionBar) for the specific fragment
+        if (currentDestinationId == R.id.loginFragment) {
+            supportActionBar?.hide()
+        } else {
+            supportActionBar?.show()
+        }
+
+        return true
     }
 }
