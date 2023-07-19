017e6d064704039fd3edf5991c34a377a18a4c66
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-19 00:29:34 -0700

    moving forward for homescreen

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
--- app/src/main/java/com/example/quizapp/authentication/SigninFragment.kt
+++ app/src/main/java/com/example/quizapp/authentication/SigninFragment.kt
@@ -1,6 +1,5 @@
 package com.example.quizapp.authentication
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
