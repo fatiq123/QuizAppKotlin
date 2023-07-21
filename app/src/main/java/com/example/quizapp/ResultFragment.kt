687be42794c59b03d7b9c14af83e4aba6ded65b0
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-21 00:00:07 -0700

    update the code

--- app/src/main/java/com/example/quizapp/ResultFragment.kt
+++ app/src/main/java/com/example/quizapp/ResultFragment.kt
@@ -1,16 +1,21 @@
 package com.example.quizapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.quizapp.databinding.FragmentResultBinding
+import com.example.quizapp.model.QuizResult
+import com.example.quizapp.viewmodel.QuizViewModel
 
 class ResultFragment : Fragment() {
 
     private lateinit var binding: FragmentResultBinding
+    private var quizResult: QuizResult? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,18 +23,62 @@ class ResultFragment : Fragment() {
     ): View? {
         binding = FragmentResultBinding.inflate(inflater, container, false)
 
-//        // Retrieve the JSON string using Safe Args
-//        val args: ResultFragmentArgs by navArgs()
-//        val quizResult = args.quizResult
+//        // Get the quiz result data from the arguments
+//        arguments?.let {
+//            val args = ResultFragmentArgs.fromBundle(it)
+//            quizResult = args.quizResult
 //
-//        // Display the user's score and percentage
-//        binding.scoreTextView.text = getString(R.string.score, quizResult.score)
-//        binding.percentageTextView.text = getString(R.string.percentage, quizResult.percentage)
+//        }
+//
+//        // Display the score and percentage in TextViews
+//        binding.scoreTextView.text = getString(R.string.score, quizResult?.score)
+//        binding.percentageTextView.text = getString(R.string.percentage, quizResult?.percentage)
+//
+//        // Set click listener for the "Finish" button
+//        binding.btnFinish.setOnClickListener {
+//            // Navigate back to the HomeFragment
+//            findNavController().navigateUp()
+//        }
 
-        // Evaluate and display the user's selected answers and correct answers
-        // (existing code for displaying selected answers and correct answers)
+
+//
+//        // Get the result data from the QuizViewModel or any other appropriate source
+//        val result = QuizViewModel.getResult() // Replace QuizViewModel with the actual ViewModel class
+//
+//        // Display the result in TextView
+//        binding.scoreTextView.text = "Your Score is: ${result.score}/${result.totalQuestions}"
+//
+//        // Set click listener for the "Back" button
+//        binding.btnFinish.setOnClickListener {
+//            // Navigate back to the HomeFragment or the appropriate destination
+//            findNavController().popBackStack() // This will navigate back to the previous fragment
+//        }
+
+
+
+
+        // Get the score and totalQuestions from the arguments
+        val score = arguments?.getInt("score", 0) ?: 0
+        val totalQuestions = arguments?.getInt("totalQuestions", 0) ?: 0
+
+        // Display the score and percentage in TextViews
+        binding.scoreTextView.text = getString(R.string.score, score)
+        Log.i("Tag", "problem here")
+        binding.percentageTextView.text = getString(R.string.percentage, calculatePercentage(score, totalQuestions))
+
+        // Set click listener for the "Finish" button
+        binding.btnFinish.setOnClickListener {
+            // Navigate back to the HomeFragment
+            findNavController().navigateUp()
+        }
 
         return binding.root
+    }
+
+
+    // Calculate the percentage
+    private fun calculatePercentage(score: Int, totalQuestions: Int): Int {
+        return (score.toFloat() / totalQuestions.toFloat() * 100).toInt()
     }
 
 }
--- app/src/main/java/com/example/quizapp/quizfragments/MathsQuizFragment.kt
+++ app/src/main/java/com/example/quizapp/quizfragments/MathsQuizFragment.kt
@@ -86,7 +86,7 @@ class MathsQuizFragment : Fragment() {
                         binding.radioGroup.clearCheck()
                     } else {
                         // Handle navigation to the ResultFragment here
-                        val action = MathsQuizFragmentDirections.actionMathsQuizFragmentToResultFragment3()
+                        val action = MathsQuizFragmentDirections.actionMathsQuizFragmentToResultFragment3(score = result, totalQuestions = Constants.TOTAL_QUESTIONS)
                         findNavController().navigate(action)
                     }
                 }
@@ -99,42 +99,10 @@ class MathsQuizFragment : Fragment() {
             }
         }
 
+
+
         return binding.root
 
-
-//
-//
-//        // since we will reset the quiz many times so
-//        Constants.RESULT = 0
-//        Constants.TOTAL_QUESTIONS = 0
-//
-//
-//
-//
-//
-//
-//
-//        // Initialize recyclerView
-//        recyclerView = view.findViewById(R.id.mathQuizView)
-//        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-//
-//
-//
-//
-//
-//
-//        // Sample list of questions (replace with actual data retrieved from API)
-//        val questions = getSampleQuestions()
-//
-//        // Set up the RecyclerView adapter
-//        questionAdapter = QuestionAdapter(questions)
-//        recyclerView.adapter = questionAdapter
-//
-//
-//
-//
-//
-//        return view
 
     }
 
@@ -153,80 +121,10 @@ class MathsQuizFragment : Fragment() {
             // For example, show a message or disable the "Next" button
             // You can also navigate to the ResultFragment here if needed
             // For example:
-            val action = MathsQuizFragmentDirections.actionMathsQuizFragmentToResultFragment3()
+            val action = MathsQuizFragmentDirections.actionMathsQuizFragmentToResultFragment3(score = result, totalQuestions = Constants.TOTAL_QUESTIONS)
             findNavController().navigate(action)
         }
     }
-
-//    @OptIn(DelicateCoroutinesApi::class)
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//
-//
-//    }
-
-
-//    @OptIn(DelicateCoroutinesApi::class)
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//
-//        // displaying first question
-//        GlobalScope.launch(Dispatchers.Main) {
-//
-//            quizViewModel.getQuestionsFromLiveData().observe(viewLifecycleOwner, Observer {
-//
-//                if (it.isNotEmpty()) {
-//                    questionsList = it
-//                    setupQuestion()
-//                    setupNextButton()
-//                }
-//            })
-//
-//            binding.btnNext.setOnClickListener {
-//                val selectedOption = binding.radioGroup.checkedRadioButtonId
-//
-//                if (selectedOption != -1) {
-//                    val radioButton = view.findViewById<View>(selectedOption) as RadioButton
-//
-//                    questionsList.let {
-//                        if (currentQuestionIndex < it.size) {
-//                            // checking if it is correct or not
-//                            if (radioButton.text.toString() == it[currentQuestionIndex].correct_option) {
-//                                result++
-//                                binding.tvResult.text = "Correct Answer: $result"
-//                            }
-//
-//                            currentQuestionIndex++
-//                            setupQuestion()
-//
-//                            // checking if it is the last question
-//                            if (currentQuestionIndex == it.size - 1) {
-//                                binding.btnNext.text = "Finish"
-//                            }
-//
-//                            binding.radioGroup.clearCheck()
-//                        } else {
-//                            // Handle quiz finish here
-//                            // You can navigate to the result fragment or activity here
-//                        }
-//                    }
-//                } else {
-//                    Toast.makeText(
-//                        requireContext(),
-//                        "Please Select One Option",
-//                        Toast.LENGTH_LONG)
-//                        .show()
-//                }
-//            }
-//
-//
-//
-//        }
-//
-//
-//    }
 
 
 
@@ -250,52 +148,6 @@ class MathsQuizFragment : Fragment() {
     }
 
 
-
-
-//    private fun setupQuestion() {
-//        if (currentQuestionIndex < questionsList.size) {
-//            val currentQuestion = questionsList[currentQuestionIndex]
-//            binding.tvQuestion.text = currentQuestion.question
-//            binding.option1.text = currentQuestion.option1
-//            binding.option2.text = currentQuestion.option2
-//            binding.option3.text = currentQuestion.option3
-//            binding.option4.text = currentQuestion.option4
-//            binding.tvResult.text = ""
-//        }
-//    }
-
-//    private fun setupNextButton() {
-//        binding.btnNext.setOnClickListener {
-//            val selectedOption = binding.radioGroup.checkedRadioButtonId
-//
-//            if (selectedOption != -1) {
-//                val radioButton = view?.findViewById<RadioButton>(selectedOption)
-//                val selectedAnswer = radioButton?.text.toString()
-//                val currentQuestion = questionsList[currentQuestionIndex]
-//
-//                // Check if the selected answer is correct
-//                if (selectedAnswer == currentQuestion.correct_option) {
-//                    result++
-//                }
-//
-//                // Move to the next question or finish the quiz
-//                if (currentQuestionIndex < questionsList.size - 1) {
-//                    currentQuestionIndex++
-//                    setupQuestion()
-//                } else {
-//                    // Finish the quiz and navigate to the result screen
-//                    navigateToResultFragment()
-//                }
-//
-//                // Clear the selected answer
-//                binding.radioGroup.clearCheck()
-//            } else {
-//                Toast.makeText(requireContext(), "Please Select One Option", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//        }
-//
-//    }
         private fun navigateToResultFragment() {
             val quizResult = QuizResult(
                 score = result,
@@ -306,26 +158,6 @@ class MathsQuizFragment : Fragment() {
 
 
         }
-
-    private fun redirectToQuiz(selectedCategory: String) {
-        // Replace this with your logic to redirect the user to the selected quiz
-        // For example, you can use Navigation Component to navigate to the quiz fragment
-        // and pass the selectedCategory as an argument.
-        // val action = HomeFragmentDirections.actionHomeFragmentToQuizFragment(selectedCategory)
-        // navController.navigate(action)
-
-        when (selectedCategory) {
-            R.id.btnMathsQuizResult.toString() -> {
-                val action = MathsQuizFragmentDirections.actionMathsQuizFragmentToResultFragment3()
-                findNavController().navigate(action)
-            }
-
-        }
-    }
-
-
-
-
 
 
     // Function to calculate the user's score based on selected answers and correct answers
