62bd0ce56f17d4ec99a5df38e82ca7e89e45d0f1
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-22 05:43:15 -0700

    working on about fragment

--- app/src/main/java/com/example/quizapp/AboutFragment.kt
+++ app/src/main/java/com/example/quizapp/AboutFragment.kt
@@ -1,59 +1,64 @@
 package com.example.quizapp
 
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.example.quizapp.databinding.FragmentAboutBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AboutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentAboutBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+
+        // Set your profile picture in a circular shape
+        val profileImage: ImageView = binding.profileImage
+        val profileImageDrawable: Drawable? = ContextCompat.getDrawable(requireContext(), R.drawable.profile_picture)
+        if (profileImageDrawable != null) {
+            profileImage.setImageDrawable(profileImageDrawable)
+            profileImage.clipToOutline = true
+        }
+
+        // Set your name and developer title
+        binding.tvName.text = "Fatiq Hussnain"
+        binding.tvDeveloperTitle.text = "Mobile App Developer"
+
+        // Set your contact details (phone number and email)
+        binding.tvPhoneNumber.text = "Phone: 03229549909"
+        binding.tvEmail.text = "Email: fatiqhussnain1@gmail.com"
+
+        // Set your app description
+        binding.tvAppDescription.text = "Hi there! I'm Fatiq Hussnain, a passionate and dedicated software developer with a strong interest in mobile app development and a love for coding. Ever since I discovered the fascinating world of programming, I've been on an exciting journey to explore the realms of technology and create meaningful solutions."
+
+        // Set up social media links (e.g., LinkedIn and GitHub)
+        binding.btnLinkedIn.setOnClickListener {
+            openLinkInBrowser("https://www.linkedin.com/in/fatiq-hussnain-7aa393201/")
+        }
+
+        binding.btnGitHub.setOnClickListener {
+            openLinkInBrowser("https://github.com/fatiq123")
+        }
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AboutFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+
+    // Function to open links in a browser
+    private fun openLinkInBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
+
 }
