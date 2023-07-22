8d855cc1c434014c9f51926af1b1e2cecdc8aa65
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-07-22 00:23:23 -0700

    working on history fragment

--- app/src/main/java/com/example/quizapp/quizfragments/HistoryFragment.kt
+++ app/src/main/java/com/example/quizapp/quizfragments/HistoryFragment.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
 
+    private lateinit var binding: FragmentHistoryBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 
